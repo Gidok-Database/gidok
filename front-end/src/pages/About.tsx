bc8795556b84316c81267cab449af7254b6195ff
kar7mp5 <tommy1005a@gmail.com>
2025-04-06 03:02:33 +0900

    feat: Initialize Home Page

---
+++ front-end/src/pages/About.tsx
@@ -0,0 +1,9 @@
+export default function About() {
+    return (
+      <div>
+        <h2>ℹ️ About Page</h2>
+        <p>This is a simple Vite + React + TS router demo.</p>
+      </div>
+    )
+  }
+  
