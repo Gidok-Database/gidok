8640652a45bb9c845bb77aa0b1f3658f5a939529
kar7mp5 <tommy1005a@gmail.com>
2025-05-13 00:18:44 +0900

    feat: Apply MarkdownRenderer

--- frontend/src/pages/Register/Register.tsx
+++ frontend/src/pages/Register/Register.tsx
@@ -8,10 +8,12 @@ export default function Register() {
         <span className="material-symbols-outlined register-icon">person_add</span>
         <h2>회원가입</h2>
         <form className="register-form">
-          <input type="text" placeholder="아이디" />
-          <input type="email" placeholder="이메일" />
-          <input type="password" placeholder="비밀번호" />
-          <input type="password" placeholder="비밀번호 확인" />
+          <input type="text" placeholder="이름" name="name" />
+          <input type="text" placeholder="아이디" name="userid" />
+          <input type="password" placeholder="비밀번호" name="password" />
+          <input type="text" placeholder="전화번호" name="phone" />
+          <input type="email" placeholder="이메일" name="email" />
+          <input type="text" placeholder="소속" name="organization" />
           <button type="submit">가입하기</button>
         </form>
         <div className="login-link">
