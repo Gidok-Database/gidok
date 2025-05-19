12614e5d7f0f6169254c852061bb2266f9fba7aa
kar7mp5 <tommy1005a@gmail.com>
2025-05-19 23:49:17 +0900

    feat(backend): 미들웨어 정책 추가

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,7 +1,16 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from routes import login, project, commit
 
 app = FastAPI(root_path="/api")
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # 또는 "*" 개발 중엔 허용
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(login.router, prefix="/user")
 app.include_router(project.router, prefix="/project")
